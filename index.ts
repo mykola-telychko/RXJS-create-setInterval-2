ea74c47d6bbdaad9e4f7ca0d50664574c1d10185
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 19:24:11 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { Observable } from 'rxjs';
+
+/*
+  Increment value every 1s, emit even numbers.
+*/
+const evenNumbers = Observable.create(function (observer) {
+  let value = 0;
+  const interval = setInterval(() => {
+    if (value % 2 === 0) {
+      observer.next(value);
+    }
+    value++;
+  }, 1000);
+
+  return () => clearInterval(interval);
+});
+//output: 0...2...4...6...8
+const subscribe = evenNumbers.subscribe((val) => console.log(val));
+//unsubscribe after 10 seconds
+setTimeout(() => {
+  subscribe.unsubscribe();
+}, 10000);
